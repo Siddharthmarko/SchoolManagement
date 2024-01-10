43e6f5d9b9ce815e95ad1f53287d43b369672f45
Siddharth <siddhartha.marko.3@gmail.com>
2024-01-10 15:27:53 +0530

    made changes

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -2,24 +2,27 @@ import React, { useEffect, useState } from "react";
 import {Link, NavLink, Outlet } from "react-router-dom";
 
 const url = 'http://localhost:3000/GroupAdmin';
+export async function fetchData() {
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        return data[userId];
+    } catch (err) {
+        console.log(err);
+        return [];
+    }
+}
+
 const userId = 0;
 export default function Home(){
     const [hidden, setHidden] =  useState(true);
     const [userData, setUserData] = useState([]);
 
-    async function fetchData(){
-    try{
-        const res = await fetch(url);
-        const data = await res.json();
-        return data[userId];
-    }catch ( err){
-        console.log(err);
-        return [];
-    }
-  }
+   
     useEffect(() => {
         fetchData().then((data) => 
         setUserData(data))
+        console.log("from hoem ", userData);
     }, []);
 
     // this sublist need to improve yet it only good for one group
@@ -36,17 +39,21 @@ return (
                         {(userData.Groups) ? userData.Groups.map((data, idx) => {
                             
                         return <li key={data.id} className="opcion-con-desplegable group">
-                            <Link to="group" state={{user: userData, index: idx}} onClick={(e) => sub_list(e)} className="text-white flex items-center justify-between p-2 hover:bg-gray-700">
-                            <div className="flex items-center">
+                            <NavLink to="group" state={{user: userData, index: idx}} onClick={(e) => sub_list(e)} className="flex items-center justify-between p-2 hover:bg-gray-700">
+                                <div className="flex   text-white items-center">
                                 <span>{data.Name}</span>
                             </div>
                             <i className="fas fa-chevron-down text-xs" />
-                            </Link>
+                            </NavLink>
                             <ul className="desplegable ml-4 ">
-                            {data.Schools.map((data) => {
+
+                            {data.Schools.map((data, schoolIdx) => {
+
                                 return <li key={data.id} style={{display : (hidden) ? 'none' : 'block'}} className=" child-list">
+
                                         <NavLink
-                                        to={`school/Id`}
+                                            to="school" 
+                                            state={{ user: userData, groupIdx: idx, index: schoolIdx }}
                                             className="block p-2 text-white hover:bg-gray-700 flex items-center"
                                         >
                                             <i className="fas fa-chevron-right mr-2 text-xs" />
