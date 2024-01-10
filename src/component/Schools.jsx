b08eb90d8a4f1d322816b6eb048d4dcef41a5a84
Siddharth <siddhartha.marko.3@gmail.com>
2024-01-10 15:26:32 +0530

    Add this school page

--- src/component/Schools.jsx
+++ src/component/Schools.jsx
@@ -1,83 +1,63 @@
 import React from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useLocation, useParams } from 'react-router-dom'
 import { allschools } from '../api/api'
 
 export default function Schools() {
-    
+    // state = {{ user: userData, groupIdx: idx, index: schoolIdx }}
+    let { state } = useLocation();
+    let schoolsData = state.user.Groups[state.groupIdx].Schools[state.index];
+    // console.log(schoolsData);
     return (
         
         <div className="flex flex-col">
             <div className="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
                 <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
                     <div className="overflow-hidden">
-                        <table className="min-w-full">
-                            <thead className="bg-white border-b">
-                                <tr>
-                                    <th
-                                        scope="col"
-                                        className="text-xl font-medium text-gray-900 px-6 py-4 text-left"
+                        <div className="flex flex-col">
+                            <div className="flex justify-between mb-4">
+                                <div className="text-xl text-gray-900 font-medium">
+                                    <Link
+                                        to="add"
+                                        // state={{ userData: state.user, id: null }}
+                                        state={{...state, index : null}}
+                                        className="text-sm bg-slate-600 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded-full"
                                     >
-                                        Schools Name
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="text-xl font-medium text-gray-900 px-6 py-4 text-left"
-                                    >
-                                        Schools Address
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="text-xl font-medium text-gray-900 px-6 py-4 text-left"
-                                    >
-                                        Schools ContactNo.
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="text-xl font-medium text-gray-900 px-6 py-4 text-left"
-                                    >
-                                        {/* <Link to={`/addschool/${id}/${-1}`} className="text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                                            Add school
-                                        </Link> */}
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="text-xl font-medium text-gray-900 px-6 py-4 text-left"
-                                    >
-                                        {/* <Link to={`/`} className="text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                                            Back to Group
-                                        </Link> */}
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-
-                                    <tr 
-                                     className="bg-white border-b">
-                                        <td className="text-xl text-gray-900 font-light px-6 py-4 text-left ">
-                                            <strong></strong>
-                                        </td>
-                                        <td className="text-xl text-gray-900 font-light px-6 py-4 text-left ">
-                                            <strong></strong>
-                                        </td>
-                                        <td className="text-xl text-gray-900 font-light px-6 py-4 text-left ">
-                                            <strong></strong>
-                                        </td>
-                                       {/* use other than div here */} 
-                                        <div>
-                                            <td className="text-xl text-gray-900 font-light py-4 text-left">
-                                                {/* <Link
-                                                    to={`/addschool/${id}/${idx}`}
-                                                    className="text-sm bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
-                                                >
-                                                    Edit
-                                                </Link> */}
-                                            </td>
+                                        Add schools
+                                    </Link>
+                                </div>
+                            </div>
+                            {schoolsData && (
+                                <div key={schoolsData.id} className="bg-white border mb-4 p-4">
+                                    <div className="flex justify-between mb-4">
+                                        <div className="text-xl text-gray-900 font-medium">
+                                            Group Name: <strong>{schoolsData.Name}</strong>
                                         </div>
+                                    </div>
+                                    <div className="flex justify-between mb-4">
+                                        <div className="text-xl text-gray-900 font-medium">
+                                            Group Address: <strong>{schoolsData.Address}</strong>
+                                        </div>
+                                    </div>
+                                    <div className="flex justify-between">
+                                        <div className="text-xl text-gray-900 font-medium">
+                                            Contact No.: <strong>{schoolsData.ContactNo}</strong>
+                                        </div>
+                                        <div className="flex space-x-4">
+                                            <Link
+                                                to="add"
+                                                // state={{ userData: state.user, id: state.index }}
+                                                state={state}
+                                                className="text-sm bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
+                                            >
+                                                Edit
+                                            </Link>
+                                        
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
 
-
-                                    </tr>
-                            </tbody>
-                        </table>
                     </div>
                 </div>
             </div>
