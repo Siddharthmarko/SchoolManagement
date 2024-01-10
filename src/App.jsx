284005d79293097f9e2eda0f0956332f4b3f7e76
Siddharth <siddhartha.marko.3@gmail.com>
2024-01-10 15:04:48 +0530

    adding forms

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,22 @@ import Home from "./component/home"
 import New from "./component/newForm"
 import Layout from "./component/layout"
 import Default_admin from "./component/default_admin"
+import NewForm from './component/newForm'
+import SchoolForm from "./component/schoolForm"
 
 function App() {
-  
+
   return (
     <div className="App">
       <Routes>
-        
         <Route path="/" element={<Home/>} >
           <Route index element={<Default_admin />} />
           <Route path="group" element={<Groups/>}/>
-          <Route path="school/:id" element={<Schools />} />
+          <Route path="group/add" element={<NewForm />} />
+          <Route path="school" element={<Schools />} />
+          <Route path="school/add" element={<SchoolForm />} />
         </Route>
-      </Routes>
+      </Routes>  
     </div>
   )
 }
