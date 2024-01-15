e41565335b119c4d6bf6a34f296f909f9a5525a8
Siddharth <siddhartha.marko.3@gmail.com>
2024-01-15 22:32:09 +0530

    new way

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,134 @@
-import { Routes, Route } from "react-router-dom"
-import Groups from './component/Groups'
-import Schools from "./component/Schools"
-import AddSchools from "./component/AddSchool"
-import { useState } from "react"
-import Home from "./component/home"
-import New from "./component/newForm"
-import Layout from "./component/layout"
-import Default_admin from "./component/default_admin"
-import NewForm from './component/newForm'
-import SchoolForm from "./component/schoolForm"
+import React, { useState, useEffect, useReducer, useContext } from "react"
 import UserContextProvider from './context/UserContextProvider'
+import UserContext from './context/UserContext'
+
+function Form({data}){
+  
+  let info = {}
+  function submitSchool(){
+    data.School.push(info);
+  }
+  console.log(data);
+  function submitGroup(){
+    info.School = []; 
+    data.push(info);
+  }
+
+function edit(e, prop){
+  info = {
+    ...info,
+      [prop] : e.target.value,
+    }
+}
+
+  return (
+    <div>
+      <input onChange={(e) => edit(e, 'Name')} type="text" value={data?.Name} />
+      <input onChange={(e) => edit(e, 'Number')} type="text" value={data?.Number} />
+      <input onChange={(e) => edit(e, 'Hidden')} type="text" value={data?.Hidden} />
+      <button onClick={() => (data?.School) ? submitSchool () : submitGroup()} > submit</button>
+    </div>
+  )
+}
+
+function Name({ hasList = false, data, nextlList }){
+const [hide, setHide] = useState(false);
+const [edit, setEdit] = useState(false);
+const [addArray, setAddArray] = useState(false);
+
+// console.log(data);
+return (
+  <div className="list" >
+    <h2>Heading </h2>
+    <button onClick={() => {setHide(!hide); setAddArray(false); setEdit(false)}}>Details</button>
+    <button onClick={() => setAddArray(!addArray)} >Add school</button>
+    <button onClick={()=> {setEdit(true); setAddArray(false); setHide(false)}} >Edit This</button>
+    {!hasList ? '' : <button onClick={ () => nextlList()} >Next list</button>}  
+  {
+    (addArray)
+    ? <Form data={data.School} />
+        : <div hidden={hide}>
+          {
+            (!edit)
+              ? <div>
+                <h2>Name : {data?.Name}</h2>
+                <h2>Number : {data?.Number}</h2>
+                <h2>Hidden : {data?.Hidden}</h2>
+              </div>
+              : <div>
+                <input onChange={(e) => edit(e)} type="text" value={data?.Name} />
+                <input onChange={(e) => edit(e)} type="text" value={data?.Number} />
+                <input onChange={(e) => edit(e)} type="text" value={data?.Hidden} />
+              </div>
+          }
+
+        </div>
+  }
+  
+  </div>
+)
+}
+
+function Details(){
+  // const [demoObj, setDemoObj] = useState([]);
+  const {demoObj} = useContext(UserContext)
+  // console.log(demoObj);
+  const [secondList,setSecondList] = useState(true);
+  const [thirdList, setThirdList] = useState(true);
+  const [form, setForm] = useState(false);
+
+  console.log(demoObj);
+  function formOpen(){
+    setForm(!form);
+  }
+
+  function hideThird(){
+    setThirdList(!thirdList);
+  }
+  function hideSecond(){
+    setSecondList(!secondList);
+  }
+
+  return ( 
+    <div>
+      <button onClick={() => formOpen() } >Add Group</button>
+      {form ? <Form data={demoObj} /> : ''}
+      {
+        (demoObj.length > 0) 
+        ? demoObj.map((item) => {
+          // console.log(item);
+          let hasSchool = item.School;
+          let schoolList = (hasSchool.length >= 1) ? true : false;
+          // console.log(hasSchool.length);
+          return <ul>
+              <li>
+              <Name data={item} hasList={schoolList} nextlList={hideThird} />
+                {
+                  (hasSchool.length > 0) 
+                  ? hasSchool.map((item) => {
+
+                    return <ul hidden={thirdList} >
+                      <li>
+                        <Name data={item} hasList={false}/>
+                      </li>
+                    </ul>
+                  }) : ''
+                }
+              </li>
+          </ul>
+        })
+        : ''
+      }
+    </div>
+  )
+}
 
 function App() {
 
   return (
     <UserContextProvider>
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home/>} >
-          <Route index element={<Default_admin />} />
-          <Route path="group" element={<Groups/>}/>
-          <Route path="group/add" element={<NewForm />} />
-          <Route path="school" element={<Schools />} />
-          <Route path="school/add" element={<SchoolForm />} />
-        </Route>
-      </Routes>  
-    </div>
-  </UserContextProvider>
+        <Details />
+    </UserContextProvider>
   )
 }
 
--- src/context/UserContextProvider.jsx
+++ src/context/UserContextProvider.jsx
@@ -1,42 +1,11 @@
-import React, { useReducer, useEffect } from "react";
+import React, {useState} from "react";
 import UserContext from "./UserContext";
-import database from '../../Jsonserver/localdata.json'
-
-const userId = 0;
-const url = 'http://localhost:3000/GroupAdmin';
-function reducer(state, action) {
-    switch (action.type) {
-        case 'update':
-            return action.payload;
-        case 'initialize':
-            return action.payload; // Set the entire state based on the payload
-        default:
-            throw new Error('Unknown action: ' + action.type);
-    }
-}
 
 const UserContextProvider = ({ children }) => {
-    const [userData, dispatch] = useReducer(reducer, { name: '', age: 0 });
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const res = await fetch(url);
-                const data = await res.json();
-                const fetchedData = data[userId];
-                dispatch({ type: 'initialize', payload: fetchedData });
-            } catch (err) {
-                console.log(err);
-                // console.log("Fetch failed. Using local data instead.", err);
-                // Use local data here
-                const localData = database;
-                dispatch({ type: 'initialize', payload: localData });
-            }
-        }
+    const [demoObj, setDemoObj] = useState([]);
 
-        fetchData();
-    }, []);
     return (
-        <UserContext.Provider value={{ userData, dispatch }}>
+        <UserContext.Provider value={{ demoObj, setDemoObj }}>
             {children}
         </UserContext.Provider>
     )
