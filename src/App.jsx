28e83a2af6ac72f8a50e8875f1f7718065297a5a
Siddharth <siddhartha.marko.3@gmail.com>
2024-01-16 16:19:46 +0530

    little changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect, useContext } from "react"
 import UserContext from './context/UserContext'
 
+/* 
+  jab group add hoga to koi prop nahi bhej jayega isiliye array jiska koi  use nai bas condtion ke liye hai
+  jab schools add hoga to object bheja jayega with group index, school index 
+*/
 function Form({data = []}){
   const {demoObj, setDemoObj } = useContext(UserContext);
   const [input, setInput] = useState({});
@@ -9,12 +13,12 @@ function Form({data = []}){
     e.preventDefault();
     
     if (Array.isArray(data)) {
-       input.School = [];
+       input.School = []; // every groups will have school array
        demoObj.push(input);
        setDemoObj(demoObj)
     } else {
-      demoObj[data.id]?.School.push(input);
-      console.log(demoObj); 
+      demoObj[data.id]?.School.push(input); // inserting new school data
+      // console.log(demoObj); 
       setDemoObj(demoObj);
     }
      
@@ -49,7 +53,7 @@ function Groups({ data, showSchool }){
     if(!editable){
       demoObj[data.id] = data;
       setDemoObj(demoObj);
-      console.log(demoObj);
+      // console.log(demoObj);
     }
   }
 
@@ -62,15 +66,26 @@ function Groups({ data, showSchool }){
         <button onClick={() => setShow(!show)}>Show details</button>
         <button onClick={() => setAdd(!add)}>Add School</button>
         <button onClick={() => showSchool(data.id)}>show School</button>
-        {(add) ? <Form data={data}/> : ''} 
-        {(show) 
-        ? <div> 
-          <button onClick={() => handleEit()} >{(editable) ? 'Edit' : 'Save'}</button>
-            <h3>Details</h3>
-          <input type="text" name="Name" onChange={(e) => changeInput(e)} value={input?.Name} readOnly={editable}/>
-          <input type="text" name="Number" onChange={(e) => changeInput(e)} value={input?.Number} readOnly={editable} />
-          <input type="text" name="Hidde" onChange={(e) => changeInput(e)} value={input?.Hidden} readOnly={editable} /> 
-        </div> : ''}
+        { (add) ? <Form data={data}/> : '' } 
+        {(show)  
+          ? <div>
+
+              <button onClick={() => handleEit()} >{(editable) ? 'Edit' : 'Save'}</button>
+              <h3>Details</h3>
+
+              <input type="text" name="Name" 
+                onChange={(e) => changeInput(e)} 
+                value={input?.Name} readOnly={editable}/>
+
+              <input type="text" name="Number" 
+                onChange={(e) => changeInput(e)} 
+                value={input?.Number} readOnly={editable} />
+
+              <input type="text" name="Hidde" 
+              onChange={(e) => changeInput(e)} 
+              value={input?.Hidden} readOnly={editable} /> 
+
+            </div> : ''}
       </div>
     )
 }
@@ -113,6 +128,7 @@ export default function App(){
   const { demoObj } = useContext(UserContext);
   const [show, setShowSchool] = useState(false);
 
+  // it show school list of particular group by using id
   function showSchool(id){
     if(show === id){
       setShowSchool(false);
@@ -124,27 +140,30 @@ export default function App(){
     <div>
       <button onClick={() => setForm(!form)}>Add Group</button>
       {form ? <Form /> : ''}
+
       {
-        (demoObj?.length > 0) 
-        ? demoObj.map((data, Groupidx) => {
-          data.id = Groupidx;
-          let school = data.School;
+        // If there is a groups object
+        (demoObj?.length > 0) ? demoObj.map((data, Groupidx) => {
+             data.id = Groupidx;
+          let school = data.School; // will be used for nested list
           
           return (
             <div className="ul">
-
               <Groups data={data} showSchool={showSchool}/>
 
-              { (show === Groupidx) 
-              ? (school.length > 0) 
-              ? school.map((data, index) => {
-                data.id = index;
-                data.Groupid = Groupidx;
+              { 
+                (show === Groupidx) 
+                ? (school.length > 0)  ? school.map((data, index) => {
+                           data.id = index;
+                      data.Groupid = Groupidx;
+
                 return <div className="list">
                           <School data={data}/> 
                       </div> 
 
-              }) : 'no record' : '' }
+              }) 
+              : 'no record' 
+              : '' }
             </div>
           )
         }) : ''
